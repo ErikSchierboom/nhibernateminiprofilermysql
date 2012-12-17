14e67ce1c38af0b4b5bf77479b8cbf6e2c911683
Erik Schierboom <erik_schierboom@hotmail.com>
2012-12-17 10:12:25 +0100

    Initial commit

---
+++ NHibernate.MiniProfiler.MySql.Tests/ProfiledMySqlClientDriverTests.cs
@@ -0,0 +1,36 @@
+﻿namespace NHibernate.MiniProfiler.MySql.Tests
+{
+    using NUnit.Framework;
+
+    using StackExchange.Profiling.Data;
+
+    [TestFixture]
+    public class ProfiledMySqlClientDriverTests
+    {
+        [Test]
+        public void CreateCommandReturnsProfiledDbCommand()
+        {
+            // Arrange
+            var clientDriver = new ProfiledMySqlClientDriver();
+
+            // Act
+            var command = clientDriver.CreateCommand();
+
+            // Assert
+            Assert.IsInstanceOf<ProfiledDbCommand>(command);
+        }
+
+        [Test]
+        public void CreateConnectionReturnsProfiledDbConnection()
+        {
+            // Arrange
+            var clientDriver = new ProfiledMySqlClientDriver();
+
+            // Act
+            var connection = clientDriver.CreateConnection();
+
+            // Assert
+            Assert.IsInstanceOf<ProfiledDbConnection>(connection);
+        }
+    }
+}
---
+++ NHibernate.MiniProfiler.MySql/ProfiledMySqlClientDriver.cs
@@ -0,0 +1,23 @@
+﻿namespace NHibernate.MiniProfiler.MySql
+{
+    using System.Data;
+    using System.Data.Common;
+
+    using NHibernate.Driver;
+
+    using StackExchange.Profiling;
+    using StackExchange.Profiling.Data;
+
+    public class ProfiledMySqlClientDriver : MySqlDataDriver
+    {
+        public override IDbCommand CreateCommand()
+        {
+            return new ProfiledDbCommand(base.CreateCommand() as DbCommand, null, MiniProfiler.Current);
+        }
+
+        public override IDbConnection CreateConnection()
+        {
+            return new ProfiledDbConnection(base.CreateConnection() as DbConnection, MiniProfiler.Current);
+        }
+    }
+}
